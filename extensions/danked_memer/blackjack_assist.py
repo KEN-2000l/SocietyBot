4a0a1c7191e1009311dcfdf8fd00fcb116393691
KEN-2000l <79527273+KEN-2000l@users.noreply.github.com>
2021-07-22 12:43:07 +0400

    fix logic error in bj assist

--- extensions/danked_memer/blackjack_assist.py
+++ extensions/danked_memer/blackjack_assist.py
@@ -5,41 +5,49 @@ from discord import Message
 from botcord.functions import log
 
 
-def find_cards(string: str) -> tuple[list, int, bool]:
+def parse_cards(string):
     results = re.findall('(?<=\[`. )(\d|10|J|Q|K|A)(?=`\])', string)
     if not results:
         log(f'Error in bj card parsing: No card found: \n{string}', tag='Error')
         return [], 0, False
 
-    soft = False
-    total = 0
     for i in range(len(results)):
-        result = results[i]
-        if result in 'JQK':
+        if results[i] in 'JQK':
             results[i] = 10
-            total += 10
-        elif result == 'A':
-            results[i] = 11
-            soft = True
-        elif result in '2345678910':
+        elif results[i] == 'A':
+            results[i] = 0
+        elif results[i] in '2345678910':
             results[i] = int(results[i])
-            total += int(results[i])
         else:
             log(f'Error in bj card parsing: Unknown card {results[i]}', tag='Error')
 
-    return results, total, soft
+    return results
+
+
+def sum_cards(cards: list):
+    total = sum(cards)
+    soft = False
+
+    aces = cards.count(0)
+    for i in range(aces):
+        if total <= 10:
+            total += 11
+            soft = True
+        else:  # total >= 11
+            total += 1
+    return total, soft
 
 
 def best_move(player: int, soft: bool, dealer: int) -> str:
     if soft:
-        if player >= 8:
+        if player >= 19:
             return 's'
-        elif player == 7:
+        elif player == 18:
             if dealer >= 9:
                 return 'h'
             else:  # dealer <= 8
                 return 's'
-        else:  # player <= 6
+        else:  # player <= 17
             return 'h'
     else:  # hard
         if player >= 17:
@@ -66,15 +74,9 @@ def setup(bot):
         if message.author.id == 270904126974590976 and ('Type `h` to **hit**, type `s` to **stand**, or type `e` to **end** the game' in message.content):
             embed = message.embeds[0].to_dict()
 
-            user_value: str = embed['fields'][0]['value']
-            memer_value: str = embed['fields'][1]['value']
+            user_cards = parse_cards(embed['fields'][0]['value'])
+            dealer_cards = parse_cards(embed['fields'][1]['value'])
 
-            _, user_total, user_soft = find_cards(user_value)
-            memer_card, _, _ = find_cards(memer_value)
-            memer_card = memer_card[0]
-            await message.channel.send(f'Your total: **`{"A+" if user_soft else ""}{user_total}`** | Dealer top card: **`{memer_card}`** \nYou should: **{best_move(user_total, user_soft, memer_card)}**')
-
-    @bot.listen('on_message')
-    async def job_assist(message: Message):
-        # todo
-        pass
+            user_total, user_soft = sum_cards(user_cards)
+            dealer_top = dealer_cards[0]
+            await message.channel.send(f'Your total: **`{"A+" + str(user_total - 11) if user_soft else user_total}`** | Dealer top card: **`{dealer_top}`** \nYou should: **{best_move(user_total, user_soft, dealer_top)}**')
